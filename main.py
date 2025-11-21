2f03c87c549cb796a23773ef3deef5b44cd4d859
thunder1o1 <matias.azar@education.nsw.gov.au>
2025-11-21 14:48:21 +1100

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import pygame as p
 import time as t
 
 #region init
+W, H = 800, 600
+
 p.init()
-r = p.display.set_mode((800, 600))
+r = p.display.set_mode((W, H), p.RESIZABLE)
 p.display.set_caption('Game Hub')
 
-C = 0
-c = 0
 F = p.font.Font('freesansbold.ttf', 48)
 FRAME = 0
+hoverOver = False
 #endregion
 
 #region text
@@ -24,6 +25,8 @@ Tplay = Text('PLAY', (400, 300), '#EEEEEE')
 #endregion
 
 #region functions
+C = 0
+c = 0
 def bg():
     global C
     C = C + .125 if C < 32 else -C
@@ -31,17 +34,21 @@ def bg():
     for i in range(60):
         h = (c + i * 2) % 255
         p.draw.rect(r, (h, h + 32, h + 40),
-                    [0, i * 10, 800, 10])
+                    [0, i * (H // 60), W, H])
 
 def buttons():
-    global text1, rect1
+    global hoverOver
     m = p.mouse.get_pos()
-    if 300 < m[0] < 500 and 200 < m[1] < 400:
-        p.draw.rect(r, (255, 0, 0), [300, 200, 200, 200])
-    else:
+    x = (FRAME % (W - 120) if FRAME % (2 * (W - 240)) < (W - 120) else (W - 120) - FRAME % (W - 120))
+    y = (FRAME % (H - 36) if FRAME % (2 * (H - 36)) < (H - 36) else (H - 36) - FRAME % (H - 36))
+    if x < m[0] < x + 120 and y < m[1] < y + 36:
+        hoverOver = True
         r.blit(Tplay.copy, Tplay.rect)
-        Tplay.rect.center = ((FRAME % 680 if FRAME % 1360 < 680 else 680 - FRAME % 680) + 60,
-                            (FRAME % 564 if FRAME % 1128 < 564 else 564 - FRAME % 564) + 36)
+        Tplay.rect.center = (x + 60, y + 18)
+    else:
+        hoverOver = False
+        r.blit(Tplay.copy, Tplay.rect)
+        Tplay.rect.center = (x + 60, y + 18)
 #endregion
 
 running = True
@@ -49,8 +56,17 @@ while running:
     for event in p.event.get():
         if event.type == p.QUIT:
             running = False
+
+        elif event.type == p.MOUSEBUTTONUP:
+            if hoverOver:
+                print('go to play screen')
+            else:
+                ()
+        
+        elif event.type == p.VIDEORESIZE:
+            W, H = r.get_width(), r.get_height()
     
-    FRAME += 2
+    FRAME += 1
     bg()
     buttons()
     p.display.update()
